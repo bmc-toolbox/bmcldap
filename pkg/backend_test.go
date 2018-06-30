199cb164415372d4a2ac0a10215a0772831fd481
Joel Rebello <joel.rebello@booking.com>
2018-06-30 11:31:38 +0200

    Add backend tests

---
+++ pkg/backend_test.go
@@ -0,0 +1,99 @@
+package pkg
+
+import (
+	"github.com/samuel/go-ldap/ldap"
+	"github.com/sirupsen/logrus"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+type invalidSession struct{}
+
+//setup requirements for test
+func setup() (*Config, *logrus.Logger) {
+	return &Config{
+		BaseDN:              "ou=People,dc=example,dc=com",
+		ClientCaCert:        "/etc/openldap/cacerts/cacert.pem",
+		RemoteServerName:    "ldaps.example.com",
+		RemoteServerPortTLS: 636,
+		Debug:               true,
+		PortTLS:             443,
+		PortInsecure:        386,
+		Cert:                "/etc/bmcldap/server.pem",
+		Key:                 "/etc/bmcldap/server-key.pem",
+		AuthorizedDNs: map[string]string{
+			"bmcUsers":  "cn=bmcUsers,ou=Group,dc=example,dc=com",
+			"bmcAdmins": "cn=bmcAdmins,ou=Group,dc=example,dc=com",
+		},
+	}, logrus.New()
+
+}
+
+//Test connect
+func TestBmcLdap_Connect(t *testing.T) {
+	config, logger := setup()
+
+	Convey("Given a ldap server", t, func() {
+		server := NewLdapServer(logger, config)
+		So(server, ShouldNotBeNil)
+
+		Convey("When there is a new connection", func() {
+			ctx, err := server.Connect(nil)
+			Convey("A new session is created", func() {
+				So(err, ShouldBeNil)
+
+				session, ok := ctx.(*session)
+				So(ok, ShouldBeTrue)
+				So(session, ShouldNotBeNil)
+			})
+		})
+	})
+}
+
+//Test Bind
+func TestBmcLdap_Bind(t *testing.T) {
+
+	config, logger := setup()
+	Convey("Given a ldap server", t, func() {
+		server := NewLdapServer(logger, config)
+		Convey("When there is a bind request with a invalid session,", func() {
+			id, err := server.Bind(&invalidSession{}, &ldap.BindRequest{DN: ""})
+			Convey("An error should be returned", func() {
+				So(err, ShouldEqual, errInvalidSessionType)
+				So(id, ShouldBeNil)
+			})
+		})
+	})
+}
+
+//Test Search
+func TestBmcLdap_Search(t *testing.T) {
+	config, logger := setup()
+	Convey("Given a ldap server", t, func() {
+		server := NewLdapServer(logger, config)
+		Convey("When there is a search request with an invalid session, ", func() {
+
+			filter := &ldap.EqualityMatch{
+				Attribute: "memberUid",
+				Value:     []byte("foo"),
+			}
+
+			searchRequest := ldap.SearchRequest{
+				BaseDN:       "foo",
+				Scope:        ldap.ScopeWholeSubtree,
+				DerefAliases: ldap.DerefAlways,
+				SizeLimit:    0,
+				TimeLimit:    0,
+				TypesOnly:    false,
+				Filter:       filter,
+				Attributes:   map[string]bool{"foo": true},
+			}
+
+			id, err := server.Search(&invalidSession{}, &searchRequest)
+			Convey("An error should be returned", func() {
+				So(err, ShouldEqual, errInvalidSessionType)
+				So(id, ShouldBeNil)
+			})
+		})
+	})
+}
